27feecea7746d1e271391a621da18fa1ca21c699
daylane <daylane86@gmail.com>
2022-04-27 19:41:52 -0300

    criando lista

--- KingsPhone/app.js
+++ KingsPhone/app.js
@@ -1,1 +1,21 @@
-foda-se
+
+var cellphone =  phone.module('kingsPhone',[])
+
+    kingsPhone.controller('PhoneListController', function PhoneListController(list) {
+        list.phone = [
+            {
+                name: 'Nokia 6310',
+                snippet: 'Celular modelo nokia tijolão, um classico.'
+              }, {
+                name: 'Windows phone: com Wi-Fi',
+                snippet: 'Modelo de celular utilizando o sistema windows.'
+              }, {
+                name: 'Redmi 10 64, xaiomi ',
+                snippet: 'Modelo de smarthphone da xaiomi com 4 cameras.'
+              }
+
+        ];
+
+    });
+
+
