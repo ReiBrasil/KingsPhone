ed7a382c412a12a6d20ca9992af2b94a90139515
ReiBrasil <103700348+ReiBrasil@users.noreply.github.com>
2022-04-29 20:54:49 -0300

    4 phones

--- KingsPhone/app.js
+++ KingsPhone/app.js
@@ -9,6 +9,9 @@ angular.module('kingsPhone', [])
                     "img/flip-phone.jpg" 
                     
                 ], 
+                "name": "Nokia 6310, Celular modelo nokia tijolão - um classico"
+                "name": "Windows phone: com Wi-Fi Modelo de celular utilizando o sistema windows"
+                "name": "Redmi 10 64, xaiomi Modelo de smarthphone da xaiomi com 4 cameras"
                 "name": "Celular Flip Vita Dual Chip MP3 Azul Multilaser - P9020"
             }
         ];
